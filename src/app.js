5fdc4e13ba35a1c4cdf67837322cdc285cc8786d
Dexter Berend <dexterjberend@gmail.com>
2021-10-10 23:12:45 -0500

    RESTful APIs

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const app = express();
 const counts = require("./data/counts-data");
 const flips = require("./data/flips-data");
 
+// simple yet important step
+// adding middleware to parsse incoming requests that contain JSON payloads
+app.use(express.json());
+
 app.use("/counts/:countId", (req, res, next) => {
   const { countId } = req.params;
   const foundCount = counts[countId];
@@ -30,8 +34,29 @@ app.use("/flips/:flipId", (req, res, next) => {
   }
 });
 
-app.use("/flips", (req, res) => {
+// 2. modified to handle only get requests
+app.get("/flips", (req, res) => {
   res.json({ data: flips });
+});
+
+// 3. added a post handler
+// Variable to hold the next ID
+// Because some IDs may already be used, find the largest assigned ID
+let lastFlipId = flips.reduce((maxId, flip) => Math.max(maxId, flip.id), 0);
+
+app.post("/flips", (req, res, next) => {
+  const { data: { result } = {} } = req.body;
+  if (result) {
+    const newFlip = {
+      id: ++lastFlipId, // Incremenet las ID, then assign as the current ID
+      result,
+    };
+    flips.push(newFlip);
+    counts[result] = counts[result] + 1; // Increment the counts
+    res.status(201).json({ data: newFlip });  // chained a call to status() to set the status
+  } else {
+    res.sendStatus(400);  // sends an error status code of 400
+  }
 });
 
 // Not found handler
